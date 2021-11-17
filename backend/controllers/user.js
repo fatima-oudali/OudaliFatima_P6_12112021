7d331027268f12b04fec61a72faa7b8e90bb3917
fatima <oud.tima@hotmail.com>
2021-11-17 03:13:52 +0100

    token & middleware auth

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 
 //l'enregistrement de nouveaux utilisateurs
@@ -31,7 +32,11 @@ exports.login = (req, res, next) => {
             }
             res.status(200).json({
                 userId: user._id,
-                token: 'TOKEN'
+                token: jwt.sign (
+                    {userId: user._id},
+                    'RANDOM_TOKEN_SECRET',
+                    {expiresTn: '24h'}
+                )
             })
         })
         .catch(error => res.status(500).json({error}));
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
 
-router.post("/", sauceCtrl.createSauce);
-router.put("/:id", sauceCtrl.modifySauce);
-router.delete("/:id", sauceCtrl.deleteSauce);
-router.get("/:id", sauceCtrl.getOneSauce);
-router.get("/", sauceCtrl.getAllSauces);
+router.post("/", auth, sauceCtrl.createSauce);
+router.put("/:id", auth, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.get("/", auth, sauceCtrl.getAllSauces);
 
 module.exports = router;
